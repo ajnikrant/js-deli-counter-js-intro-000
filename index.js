e936b44101ef7f276d8adc0d24a3fd54af3748c5
Amanda Nikrant <ajnikrant@gmail.com>
2020-12-29 18:27:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ function nowServing(theDeliLine) {
 
 
 
-function currentLine(theDeliLine, customers) {
+function currentLine(theDeliLine) {
   if (theDeliLine.length > 0){
     for (let i = 0; i < customers.length; i++) {
         return `The line is currently` + (i+1) + `${customers[i]}.`;
       }
-      return theDeliLine;
   }
 
   else {
