e3c6752287c429fad59311a249b1da579a24ba6c
Amanda Nikrant <ajnikrant@gmail.com>
2020-12-29 17:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(theDeliLine, name) {
   //let theDeliLine = [];
 
   if (theDeliLine.length > 0){
-    return "Currently serving ${name}";
+    return "Currently serving `${name}`";
   }
 
   else {
